9b56d15bcf1fc60a908c016ae90af11e80e66758
Trabalho <Trabalho@DESKTOP-ADR2II7>
2017-03-16 09:32:29 -0300

    Arrumando o redirecionamento do LoginFilter, agora redireciona estando em qualquer pagina e alterando a pagina de listar aluno, inserindo a modal.

--- Catalogo-tg/src/main/java/org/fatec/filter/LoginFilter.java
+++ Catalogo-tg/src/main/java/org/fatec/filter/LoginFilter.java
@@ -16,27 +16,38 @@ import javax.servlet.http.HttpSession;
 import org.fatec.model.Usuario;
 public class LoginFilter implements Filter {
 	
-	ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+	
 	private final static String FILTER_APPLIED = "_security_filter_applied";
+	
+	ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+	
+	
 	public LoginFilter() {
+			
 	}
+	
 	public void init(FilterConfig arg0) throws ServletException {
+			
 	}
+	
 	public void destroy() {
+			
 	}
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+	
+	@SuppressWarnings("deprecation")
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
 		HttpServletRequest hreq = (HttpServletRequest) request;
 		HttpServletResponse hresp = (HttpServletResponse) response;
 		HttpSession session = hreq.getSession();
-		hreq.getPathInfo();
+		//hreq.getPathInfo();
 		String paginaAtual = new String(hreq.getRequestURL());
+		
 		
 		// dont filter login.jsp because otherwise an endless loop.
 		// & only filter .jsp otherwise it will filter all images etc as well.
-		if ((request.getAttribute(FILTER_APPLIED) == null) && paginaAtual != null
-				&& (!paginaAtual.endsWith("index.xhtml"))
-				&& (paginaAtual.endsWith(".xhtml"))) {
+		if ((request.getAttribute(FILTER_APPLIED) == null) && paginaAtual != null && (!paginaAtual.endsWith("index.xhtml")) && (paginaAtual.endsWith(".xhtml"))) {
+			
 			request.setAttribute(FILTER_APPLIED, Boolean.TRUE);
 			// If the session bean is not null get the session bean property
 			// username.
@@ -44,11 +55,13 @@ public class LoginFilter implements Filter {
 			if ((session.getAttribute("info_user")) != null) {
 				 user = (Usuario) session.getAttribute("info_user");
 			}
+			
 			if ((user == null) || (user.equals(""))) {
-				//hreq.getRealPath("/index.xhtml");
+				String contextPath = ((HttpServletRequest) request).getContextPath();
 				
-				hresp.sendRedirect(context.getRequestContextPath() + "../../../../index.xhtml");
-				return;
+				((HttpServletResponse) response).sendRedirect(contextPath + "/index.xhtml");
+				
+                return;
 			}
 		}
 		// deliver request to next filter
